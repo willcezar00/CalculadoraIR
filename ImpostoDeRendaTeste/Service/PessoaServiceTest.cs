8fd606230fdad91ca4bbbc7ad5a496f7e41031c0
William Cezar Ribeiro <C0605062@globalvale.onmicrosoft.com>
2019-11-16 23:14:55 -0300

    reportService Test

---
+++ ImpostoDeRendaTeste/Service/PessoaServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using ImpostoDeRenda.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImpostoDeRendaTeste.Service
+{
+    [TestClass]
+    public class PessoaServiceTest
+    {
+        private static readonly string filePath = "..\\..\\..\\Resources\\input.txt";
+
+        [TestMethod]
+        public void ReadPessoasFromFileSize5Test()
+        {
+            var pessoaService = PessoaService.GetInstance();
+            var pessoas = pessoaService.ReadPessoas(filePath);
+            Assert.AreEqual(5, pessoas.Count);
+        }
+
+    }
+}
---
+++ ImpostoDeRendaTeste/Service/ReportServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using ImpostoDeRenda.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImpostoDeRendaTeste.Service
+{
+    [TestClass]
+    public class ReportServiceTest
+    {
+        private static readonly string filePath = "..\\..\\..\\Resources\\input.txt";
+
+        [TestMethod]
+        public void GerarReportSize5Test()
+        {
+            var pessoaService = PessoaService.GetInstance();
+            var pessoas = pessoaService.ReadPessoas(filePath);
+
+            var reportService = ReportService.GetInstance();
+            var reports = reportService.GerarReport(pessoas);
+
+            Assert.AreEqual(5, reports.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReadPessoasFromFileFailTest()
+        {
+            var reportService = ReportService.GetInstance();
+            var reports = reportService.GerarReport(null);
+        }
+    }
+}
