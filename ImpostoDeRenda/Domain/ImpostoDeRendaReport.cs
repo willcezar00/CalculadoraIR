28eaa97be25ff2ec9c692ed80d2cfaf18c06fe4b
William Cezar Ribeiro <C0605062@globalvale.onmicrosoft.com>
2019-11-17 12:22:59 -0300

    Teste de Aliquota

--- ImpostoDeRenda/Domain/ImpostoDeRendaReport.cs
+++ ImpostoDeRenda/Domain/ImpostoDeRendaReport.cs
@@ -7,11 +7,8 @@ namespace ImpostoDeRenda.Domain
     public class ImpostoDeRendaReport
     {
         public Pessoa Pessoa { get; set; }
-
         public double ImpostoRetido { get; set; }
-
         public double RendaTotal { get; set; }
-
         public double RendaTaxavel { get; set; }
     }
 }
---
+++ ImpostoDeRendaTeste/Domain/TipoAliquotaTest.cs
@@ -0,0 +1,45 @@
+﻿using ImpostoDeRenda.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImpostoDeRendaTeste.Domain
+{
+    [TestClass]
+    public class TipoAliquotaTest
+    {
+        [TestMethod]
+        public void IsOnAliquotaSuccessTest()
+        {
+            var renda = 2000d;
+            var tipoAliquota = TipoAliquota.ALIQUOTAS.Where(aliquota => aliquota.IsOnAliquota(renda)).First();
+            Assert.AreEqual(TipoAliquota.ALIQUOTA_NIVEL_1, tipoAliquota);
+        }
+
+        [TestMethod]
+        public void IsOnAliquotaFailTest()
+        {
+            var renda = 3000d;
+            var tipoAliquota = TipoAliquota.ALIQUOTAS.Where(aliquota => aliquota.IsOnAliquota(renda)).First();
+            Assert.AreNotEqual(TipoAliquota.ALIQUOTA_NIVEL_1, tipoAliquota);
+        }
+
+        [TestMethod]
+        public void IsOCalculateAliquotanAliquotaSuccessTest()
+        {
+            var renda = 3000d;
+            var impostoRetido = TipoAliquota.ALIQUOTAS.Where(aliquota => aliquota.IsOnAliquota(renda)).First();
+            Assert.IsTrue(impostoRetido.IsOnAliquota(renda));
+        }
+
+        [TestMethod]
+        public void IsOCalculateAliquotanAliquotaFailTest()
+        {
+            var renda = 2000d;
+            var impostoRetido = TipoAliquota.ALIQUOTAS.Where(aliquota => aliquota.IsOnAliquota(renda)).First();
+            Assert.IsFalse(impostoRetido.IsOnAliquota(3000d));
+        }
+    }
+}
